558134f88305a858096c7a2deeeb25778721d10f
Danylo Owl <d.ribchinski@gmail.com>
2021-12-01 13:30:23 -0500

    created calcBitsEntropy
    Calculates the entropy of a string via log2(unique_symbols^num_characters)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+#include <cmath>
 #include <iostream>
+#include <unordered_set>
+
+// Entropy formula is
+// S = size, U = num of unique symbols
+// bits entropy = Bs
+// Bs = log2(U^S)
+double calcBitsEntropy(const std::string& in) {
+    std::unordered_set<char> chars;
+    for (char c : in)
+        chars.insert(c);
+    double product = pow(chars.size(), in.size());
+    double bitLog = log2(product);
+    return bitLog;
+}
 
 int main() {
     std::cout << "Hello World!" << std::endl;
